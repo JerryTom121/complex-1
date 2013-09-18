763fbcadd36773aa889b1fc49208f6987065fe1a
Ivan Bogun <ibogun2010@my.fit.edu>
2013-09-17 22:08:24 -0400

    added twitterstream.py

---
+++ download/print.py
@@ -0,0 +1,6 @@
+'''
+Created on Sep 17, 2013
+
+@author: ivan
+'''
+import oauth as oauth2
---
+++ download/twitterstream.py
@@ -0,0 +1,66 @@
+'''
+Created on Sep 17, 2013
+
+@author: ivan
+'''
+import oauth2 as oauth
+import urllib2 as urllib
+
+# See Assignment 1 instructions or README for how to get these credentials
+access_token_key = "1421319247-XTrjGKTyEZu4h5duCpvT4VnWycdySzmzzgNMutP"
+access_token_secret = "gGZQ7ZJlTpZLyx6q14bssIx9qKhU9Z4e9pVERVMV8"
+
+consumer_key = "NON0gkzkjARUT5nSbHah7A"
+consumer_secret = "UOM1s70nRjOdf91dCvGKospyM0KMiJRLudATTQBmqMs"
+
+_debug = 0
+
+oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+
+signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+http_method = "GET"
+
+
+http_handler  = urllib.HTTPHandler(debuglevel=_debug)
+https_handler = urllib.HTTPSHandler(debuglevel=_debug)
+
+'''
+Construct, sign, and open a twitter request
+using the hard-coded credentials above.
+'''
+def twitterreq(url, method, parameters):
+  req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                             token=oauth_token,
+                                             http_method=http_method,
+                                             http_url=url, 
+                                             parameters=parameters)
+
+  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+
+  headers = req.to_header()
+
+  if http_method == "POST":
+    encoded_post_data = req.to_postdata()
+  else:
+    encoded_post_data = None
+    url = req.to_url()
+
+  opener = urllib.OpenerDirector()
+  opener.add_handler(http_handler)
+  opener.add_handler(https_handler)
+
+  response = opener.open(url, encoded_post_data)
+
+  return response
+
+def fetchsamples():
+  url = "https://stream.twitter.com/1/statuses/sample.json"
+  parameters = []
+  response = twitterreq(url, "GET", parameters)
+  for line in response:
+    print line.strip()
+
+if __name__ == '__main__':
+  fetchsamples()
